f0bbc52c40ce2332ef638894b955a89b4782cfcb
ReyajAlam <103191596+ReyajAlam@users.noreply.github.com>
2024-05-02 10:36:06 +0000

    second java script

---
+++ learn/second.js
@@ -0,0 +1,26 @@
+// primitive deta type
+
+// 7 type of dt
+/* string - call by value
+number
+boolean
+null
+undefined
+symbol
+BigInt */
+
+/*const score = 100
+const like = false
+const feeel = null
+let  good;*/
+
+// const give =  "reyaj"
+const  di = symbol('345')
+const  other = symbol('345')
+
+console.log(di === other)
+
+// Reference ( Non Primitive Deta type)
+
+// objects , array , Function
+
